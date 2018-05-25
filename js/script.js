0a7318e2a88b7134ecb6e0203dab57302975768c
Bartłomiej Bajda <allerbbb@gmail.com>
2018-05-25 11:40:45 +0000

    handling bugs

--- js/script.js
+++ js/script.js
@@ -17,34 +17,41 @@ var flag = "https://restcountries.eu/data/col.svg"
 
 function showCountriesList(resp) {
   countriesList.empty();
-  resp.forEach(function(item) {
+  if(resp) {
+      resp.forEach(function(item) {
 
-    var country = $('<li>').text(item.name);
-    country.appendTo(countriesList);
+      var country = $('<li>').text(item.name);
+      country.appendTo(countriesList);
+  
+      var photo = $('<p></p>');
+      var code = item.alpha3Code.toLowerCase() ;
+      var flag = 'https://restcountries.eu/data/'+ code  + '.svg'; // URL to flag img
+      var img = document.createElement('IMG');
+      img.classList.add('photo');
+      img.setAttribute('src', flag);
+      photo.html(img).appendTo(countriesList);
+  
+      var capital = $('<p></p>').html("<span>Capital: </span> " + item.capital);
+      capital.appendTo(countriesList);
+  
+      var population = $('<p></p>').html("<span>Population: </span> " + item.population);
+      population.appendTo(countriesList);
+  
+      var area = $('<p></p>').html("<span>Land area: </span> " + item.area + " km <SUP>2</SUP>" );
+      area.appendTo(countriesList);
+  
+      var currencies = $('<p></p>')
+      currencies.addClass('currencies');
+      currencies.html("<span>Currency: </span> " + item.currencies).appendTo(countriesList);
+    });
+  
+    $('#countries').find('span').addClass('property');
 
-    var photo = $('<p></p>');
-    var code = item.alpha3Code.toLowerCase() ; //zmiana na male litery
-    var flag = 'https://restcountries.eu/data/'+ code  + '.svg'; //adres zdjecia flagi
-    photo.html('<img class="photo" src="'+ flag +'"/>');
-    photo.appendTo(countriesList);
-
-    var capital = $('<p></p>').html("<span class=\"property\">Capital: </span> " + item.capital);
-    capital.appendTo(countriesList);
-
-    var population = $('<p></p>').html("<span class=\"property\">Population: </span> " + item.population);
-    population.appendTo(countriesList);
-
-    var area = $('<p></p>').html("<span class=\"property\">Land area: </span> " + item.area + " km <SUP>2</SUP>" );
-    area.appendTo(countriesList);
-
-    var currencies = $('<p class=\"currencies\"></p>').html("<span class=\"property\">Currency: </span> " + item.currencies);
-    currencies.appendTo(countriesList);
-  });
-
-   //WARUNEK NA ISTNIENIE NAPISU: "LIST OF FOUND COUNTRIES"
-   var list = $('li');
-   var listBefore = $('body').find('.beforelisttext');
-   if (list.length > 2 && (listBefore.length == 0)) {
-      countriesList.before('<p class=\"beforelisttext\">List of found countries</p>');
-   }
+     //TEXT: "LIST OF FOUND COUNTRIES"
+     var list = $('li');
+     var listBefore = $('body').find('.beforelisttext');
+     if (list.length > 2 && (listBefore.length == 0)) {
+        countriesList.before('<p class=\"beforelisttext\">List of found countries</p>');
+     }
+  }
 }
